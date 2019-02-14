ffd645ec698482e764adc8fbc48eb0978a974255
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-14 12:07:33 +0100

    contact

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [{
-  path: '', component: HomeComponent,
-}];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, ContactComponent]
 })
 export class AppRoutingModule { }
